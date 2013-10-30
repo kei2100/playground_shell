bbe0b209f2c7725b67e4e97b05aeae21dd01afe1
kei2100 <kei.arima.2100@gmail.com>
2013-10-30 15:36:15 +0900

    first commit.

--- expect_spawn_ssh.sh
+++ expect_spawn_ssh.sh
@@ -4,6 +4,7 @@ USER=$1
 HOST=$2
 PASSWORD=$3
 
+# simple
 expect <<EOF
 set timeout 10
 spawn ssh ${USER}@${HOST}
@@ -11,3 +12,44 @@ expect password: ; send "${PASSWORD}\r"
 expect "$ " ; send "hostname\r"
 expect "$ " ; send "exit\r"
 EOF
+
+# functions
+EXPECT_SSH_CMD() {
+expect <<EOF
+  set timeout -1
+  spawn ssh ${SSH_USER}@${HOST}
+  expect "password:" {
+    send "${PASSWORD}\r" 
+    expect "$ " {
+      send "${1}\r"
+      expect "$ " ; send "exit\r"
+    } "Permission denied, please try again." {
+      puts "(ERROR) Failed to login."
+      exit 1
+    }
+  } timeout {
+    puts "(ERROR) Failed to connect."
+    exit 1
+  }
+EOF
+}
+
+EXPECT_SCP_PULL() {
+expect <<EOF
+  set timeout -1
+  spawn scp ${SSH_USER}@${HOST}:${1} ${2}
+  expect "password:" {
+    send "${PASSWORD}\r" 
+    expect "$ " {
+      puts "done"
+    } "Permission denied, please try again." {
+      puts "(ERROR) Failed to login."
+      exit 1
+    }
+  } timeout {
+    puts "(ERROR) Failed to connect."
+    exit 1
+  }
+EOF
+}
+
---
+++ get_absolute_path_of_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+PATH=$(cd "$(dirname "$0")"; pwd)
+echo ${PATH}
